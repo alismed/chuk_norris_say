c3a378b88008b87c55ec43ce9d7917eaef427756
Alison Souza <alisonmsouza@yahoo.com.br>
2016-05-28 11:44:41 -0300

    bug fix

--- joke.rb
+++ joke.rb
@@ -1,28 +1,31 @@
 require 'rest-client'
 require 'json'
 require 'find'
-require 'pry'
 
   def call_api
-    apis = { icndb: "http://api.icndb.com/jokes/random", yomomma: "http://api.yomomma.info/" }
+    apis = {
+	    icndb: "http://api.icndb.com/jokes/random",
+	    yomomma: "http://api.yomomma.info/" }
     api = apis.to_a.sample
-    response = RestClient.get(api[1])
+    response = RestClient.get(api.last)
     json     = JSON.parse(response)
+
     if api.include? :icndb
       joke = json.fetch("value").fetch("joke")
     else
       joke = json.fetch("joke")
     end
-    joke
+
+    joke.gsub(/'/, "\\\\'")
   end 
 
   animals = []
-  Dir.foreach("/usr/share/cowsay/") do |file|
-    if /.+?(?=\.cow)/.match(file)
-      animals << /.+?(?=\.cow)/.match(file)
+  regx = /.+?(?=\.cow)/
+  Dir.foreach("/usr/share/cowsay/cows/") do |file|
+    if regx.match(file)
+      animals << regx.match(file)
     end
   end
 
 system "cowsay -f #{animals.sample} #{call_api}"
-
 
